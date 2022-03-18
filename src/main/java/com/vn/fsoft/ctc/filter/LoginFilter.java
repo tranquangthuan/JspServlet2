b818454cd18b6998b83ecf3b490bb49c9c6b0fe5
Thuan <thuan.uit@gmail.com>
2022-03-18 09:01:33 +0700

    add Filter

--- src/main/java/com/vn/fsoft/ctc/filter/LoginFilter.java
+++ src/main/java/com/vn/fsoft/ctc/filter/LoginFilter.java
@@ -18,8 +18,6 @@ public class LoginFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -45,8 +43,6 @@ public class LoginFilter implements Filter {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
